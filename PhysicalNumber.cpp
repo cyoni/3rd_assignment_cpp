65b6b966f9f42f14a91e2a3dfbb713063108af44
cyoni <44746539+cyoni@users.noreply.github.com>
2019-04-12 20:02:20 +0300

    Add files via upload

--- PhysicalNumber.cpp
+++ PhysicalNumber.cpp
@@ -1,49 +1,78 @@
 #include <iostream>
 #include <sstream>
 #include <stdexcept>
-#include "PhysicalNumber.h"
+#include <string.h>
 //clang++-5.0 -std=c++17 PhysicalNumber.cpp Unit.cpp
+
+#include "PhysicalNumber.h"
+
+
 
 using std::cout, std::endl, std::boolalpha, std::istringstream;
 using ariel::PhysicalNumber, ariel::Unit;
 
-    ariel::PhysicalNumber::PhysicalNumber(){ }
+    ariel::PhysicalNumber::PhysicalNumber(){   }
 
     ariel::PhysicalNumber::PhysicalNumber(double d, Unit k){
     data=d;
     switch(k){
-    ms=k;
     case Unit::CM:
+    ms = k;
     measure = "cm";
     break;
     case Unit::M:
     measure = "m";
+    ms = k;
     break;
     case Unit::KM:
     measure = "km";
+    ms = k;
     break;
     case Unit::S:
     measure = "s";
+    ms = k;
     break;
     case Unit::MIN:
     measure = "min";
+    ms = k;
     break;
     case Unit::HOUR:
     measure = "hour";
+    ms = k;
     break;
     case Unit::G:
     measure = "g";
+    ms = k;
     break;
     case Unit::KG:
     measure = "kg";
+    ms = k;
     break;
     case Unit::TON:
     measure = "ton";
+    ms = k;
     break;
     }
 
     }
 
+    Unit ariel::PhysicalNumber::getmeasure(std::string m){
+    Unit u ;
+
+    if (m == "CM") u = Unit::CM;
+    else if (m == "KM") u = Unit::KM;
+    else if (m == "M") u = Unit::M;
+    else if (m == "S") u = Unit::S;
+    else if (m == "HOUR") u = Unit::HOUR;
+    else if (m == "MIN") u = Unit::MIN;
+    else if (m == "S") u = Unit::S;
+    else if (m == "TON") u = Unit::TON;
+    else if (m == "G") u = Unit::G;
+    else if (m == "KG") u = Unit::KG;
+    else Throw("error 552");
+
+    return u;
+    }
     std::ostream& ariel::operator<<(std::ostream &os, const PhysicalNumber&  dt){
     return os << dt.get_data() << '[' << dt.get_measure() << ']';
     }
@@ -52,18 +81,65 @@ using ariel::PhysicalNumber, ariel::Unit;
     return ms;
     }
 
+
+    double PhysicalNumber::convert(ariel::Unit ms_a, ariel::Unit ms_b,std::string str1,  std::string str2, double data) const{
+    double res=0;
+
+         if (ms_a==ariel::Unit::M && ms_b == ariel::Unit::CM)   res = data/100; // CM->M
+    else if (ms_a==ariel::Unit::KM && ms_b == ariel::Unit::CM)   res = data/1000; // CM->KM
+
+    else if (ms_a==ariel::Unit::CM && ms_b == ariel::Unit::M)   res = data*100; // M->CM
+    else if (ms_a==ariel::Unit::KM && ms_b == ariel::Unit::M)   res = data/1000; // M->KM
+
+    else if (ms_a==ariel::Unit::CM && ms_b == ariel::Unit::KM)   res = data*100000; // KM->CM
+    else if (ms_a==ariel::Unit::M && ms_b == ariel::Unit::KM)   res = data*1000; // KM->M
+
+//---
+    else if (ms_a==ariel::Unit::MIN && ms_b == ariel::Unit::S)   res = data/60;
+    else if (ms_a==ariel::Unit::HOUR && ms_b == ariel::Unit::S)   res = data/3600;
+
+    else if (ms_a==ariel::Unit::S && ms_b == ariel::Unit::MIN)  {res = data*60;}
+    else if (ms_a==ariel::Unit::HOUR && ms_b == ariel::Unit::MIN)   res = data/60;
+
+    else if (ms_a==ariel::Unit::S && ms_b == ariel::Unit::HOUR)   res = data*3600;
+    else if (ms_a==ariel::Unit::MIN && ms_b == ariel::Unit::HOUR)   res = data*60;
+
+// --
+    else if (ms_a==ariel::Unit::TON && ms_b == ariel::Unit::G)   res = data/1000000;
+    else if (ms_a==ariel::Unit::KG && ms_b == ariel::Unit::G)   res = data/1000;
+
+    else if (ms_a==ariel::Unit::G && ms_b == ariel::Unit::KG)   res = data*1000;
+    else if (ms_a==ariel::Unit::TON && ms_b == ariel::Unit::KG)   res = data/1000;
+
+    else if (ms_a==ariel::Unit::KG && ms_b == ariel::Unit::TON)   res = data*1000;
+    else if (ms_a==ariel::Unit::G && ms_b == ariel::Unit::TON)   res = data*1000000;
+    else{
+
+    PhysicalNumber::Throw("Units do not match - [" +str1+ "] cannot be converted to ["+ str2 +"]");
+        }
+
+    return res;
+    }
+
     void PhysicalNumber::Throw(std::string str) const{
      throw std::invalid_argument(str);
     }
 
     const PhysicalNumber ariel::operator+(const PhysicalNumber& a, const PhysicalNumber& b){
+    double res=0;
+    if (a.getmeasure()==b.getmeasure()) res = b.get_data();
+    else
+    res = a.convert(a.getmeasure(),b.getmeasure(),a.get_measure(), b.get_measure(), b.get_data());
 
-
-    return PhysicalNumber(a.get_data(), a.getmeasure());
+    return PhysicalNumber(res+a.get_data(), a.getmeasure());
     }
 
     const PhysicalNumber ariel::operator-(const PhysicalNumber& a, const PhysicalNumber& b){
-    return PhysicalNumber(a.get_data(), a.getmeasure());
+    double res=0;
+    if (a.getmeasure()==b.getmeasure()) res = b.get_data();
+    else
+    res = a.convert(a.getmeasure(),b.getmeasure(),a.get_measure(), b.get_measure(), b.get_data());
+    return PhysicalNumber(-res+a.get_data(), a.getmeasure());
     }
 
 
@@ -72,11 +148,19 @@ using ariel::PhysicalNumber, ariel::Unit;
     }
 
     const bool ariel::operator>(const PhysicalNumber& a, const PhysicalNumber& b){
-    return false;
+    double res=0;
+    if (a.getmeasure()==b.getmeasure()) res = b.get_data();
+    else
+    res = a.convert(a.getmeasure(),b.getmeasure(),a.get_measure(), b.get_measure(), b.get_data());
+    return (a.get_data() > res);
     }
 
     const bool ariel::operator<(const PhysicalNumber& a, const PhysicalNumber& b){
-    return false;
+    double res=0;
+    if (a.getmeasure()==b.getmeasure()) res = b.get_data();
+    else
+    res = a.convert(a.getmeasure(),b.getmeasure(),a.get_measure(), b.get_measure(), b.get_data());
+    return (a.get_data() < res);
     }
 
     const PhysicalNumber ariel::operator-(const PhysicalNumber& a){
@@ -87,15 +171,27 @@ using ariel::PhysicalNumber, ariel::Unit;
     return PhysicalNumber(a.get_data(), a.getmeasure());
     }
     const bool ariel::operator<=(const PhysicalNumber& a, const PhysicalNumber& b){
-    return false;
+    double res=0;
+    if (a.getmeasure()==b.getmeasure()) res = b.get_data();
+    else
+    res = a.convert(a.getmeasure(),b.getmeasure(),a.get_measure(), b.get_measure(), b.get_data());
+    return (a.get_data() <= res);
     }
 
     const bool ariel::operator>=(const PhysicalNumber& a, const PhysicalNumber& b){
-    return false;
+    double res=0;
+    if (a.getmeasure()==b.getmeasure()) res = b.get_data();
+    else
+    res = a.convert(a.getmeasure(),b.getmeasure(),a.get_measure(), b.get_measure(), b.get_data());
+    return (a.get_data() >= res);
     }
 
     const bool ariel::operator==(const PhysicalNumber& a, const PhysicalNumber& b){
-    return false;
+    double res=0;
+    if (a.getmeasure()==b.getmeasure()) res = b.get_data();
+    else
+    res = a.convert(a.getmeasure(),b.getmeasure(),a.get_measure(), b.get_measure(), b.get_data());
+    return (a.get_data() == res);
     }
 
 
--- PhysicalNumber.h
+++ PhysicalNumber.h
@@ -5,6 +5,7 @@
 #include "Unit.h"
 
 using std::cout, std::endl, std::boolalpha, std::istringstream;
+//using ariel::Unit;
 
 namespace ariel{
 
@@ -17,10 +18,10 @@ namespace ariel{
 
     PhysicalNumber();
     PhysicalNumber(double,Unit); // constructor
-
+                                 // declarations
     bool boolalpha = false;
-    void getMeasures(std::string& g,  ariel::Unit ms_a) const;
     friend std::ostream& operator<<(std::ostream&, const PhysicalNumber&);
+   // friend std::istream& operator>> (std::istream&, PhysicalNumber&);
     friend const PhysicalNumber operator+(const PhysicalNumber&, const PhysicalNumber&);
     friend const PhysicalNumber operator-(const PhysicalNumber&, const PhysicalNumber&);
     friend const PhysicalNumber operator+(const PhysicalNumber&);
@@ -31,42 +32,103 @@ namespace ariel{
     friend const bool operator>=(const PhysicalNumber&, const PhysicalNumber&);
     friend const bool operator==(const PhysicalNumber&, const PhysicalNumber&);
     friend const bool operator!=(const PhysicalNumber&, const PhysicalNumber&);
+    double convert(Unit, Unit, std::string ,std::string , double) const;
+    double get_data() const{return data;}
+    std::string get_measure() const{return measure;}
+    Unit getmeasure() const;
+    Unit getmeasure(std::string);
+    void Throw(std::string) const;
 
-    friend std::istream& operator>> (std::istream& input, PhysicalNumber& a) {
-    return (input >> a.data);
+
+   friend std::istream& operator>> (std::istream& input, PhysicalNumber& a) {
+   // WHAT IS INPUT.GET , INPUT.PEEK   . INPUT.SETSTATE
+
+    std::string tmpStr;
+    std::string m_str;
+
+    input >>  tmpStr;
+    int index = tmpStr.find('[');
+    int index2 = tmpStr.find(']');
+
+    if (index==-1 || index2 == -1) { //
+    a.Throw("[ or ] is missing");
     }
 
+    if (index == 0) // there is nothing before the [
+    {
+    a.Throw("nothing before [");
+    }
+       // cout << "TEST1 " << tmpStr << endl;
 
-     double convert(ariel::Unit, ariel::Unit, double) const;
-     double get_data() const{return data;}
-     std::string get_measure() const{return measure;}
-     Unit getmeasure() const;
-     void Throw(std::string) const;
+    std::string newNum = tmpStr.substr(0,index);
+
+    // check what is this type of number. (str, or number, or str+number)
+   // cout << index2 << endl;
+    m_str = tmpStr.substr(index+1);
+    m_str = m_str.substr(0,m_str.length()-1);
+
+    // check if it's appropriate measure.   Capital letter?
+
+    if (m_str == "M" || m_str == "KM" || m_str == "CM" || m_str == "S" || m_str == "HOUR" || m_str == "MIN" || m_str == "TON" || m_str == "G"
+    || m_str == "KG"){
+    //cout << "TEST " << newNum << endl;
+
+    int number = 0;
+    istringstream xx(newNum);
+    xx >> number;
+
+    a = PhysicalNumber(number, a.getmeasure(m_str));
+    }
+    else{
+    a.Throw("not valid measurment");
+    }
+
+    //cout << "try to test=" << tmpStr << ',' << index << ',' << m_str << endl;
+
+    return (input >> a.data);
+
+   }
+
+
 
      PhysicalNumber& operator+=(const PhysicalNumber& b){
+     double res=0;
+     if (getmeasure()==b.getmeasure()) res = b.get_data();
+     else
+     res = convert(getmeasure(),b.getmeasure(),get_measure(), b.get_measure(), b.get_data());
+     data = data+res;
      return *this;
-     }
+    }
 
      PhysicalNumber& operator-=(const PhysicalNumber& b){
+     double res=0;
+     if (getmeasure()==b.getmeasure()) res = b.get_data();
+     else
+     res = convert(getmeasure(),b.getmeasure(),get_measure(), b.get_measure(), b.get_data());
+     data = data-res;
      return *this;
-     }
+    }
 
      PhysicalNumber& operator++(){ // prefix inc (++a)
+     data++;
      return *this;
-     }
+    }
 
     const PhysicalNumber operator++(int d) { // postfix inc (a++)
     PhysicalNumber copy = *this;
+    data++;
     return copy;
     }
 
 
     PhysicalNumber& operator--(){ // prefix inc (--a)
+    data--;
     return *this;
     }
 
     const PhysicalNumber operator--(int d) { // postfix inc (a--)
     PhysicalNumber copy = *this;
+    data--;
     return copy;
     }
 };
